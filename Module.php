38d783b5a3909fe67796d1750ff6c27432c506d3
dave-yang <daworld.ny@gmail.com>
2019-10-04 21:22:42 +0500

    Create Module.php

---
+++ Module.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Visitor;
+
+use Krystal\Application\Module\AbstractModule;
+
+final class Module extends AbstractModule
+{
+    /**
+     * Returns routes of this module
+     * 
+     * @return array
+     */
+    public function getRoutes()
+    {
+        return array(
+        );
+    }
+
+    /**
+     * Returns prepared service instances of this module
+     * 
+     * @return array
+     */
+    public function getServiceProviders()
+    {
+        return array(
+        );
+    }
+}
